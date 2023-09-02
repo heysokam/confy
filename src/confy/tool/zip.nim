af785f09980124659e9b836055ec3c2c9130eaf6
sOkam <itsfatum@gmail.com>
2023-09-03 00:08:04 +0200

    fix: Potential bugfix for unzip Zig on windows (zippy didn't overwrite)

--- src/confy/tool/zip.nim
+++ src/confy/tool/zip.nim
@@ -15,10 +15,9 @@ import ../cfg
 #     : https://github.com/tukaani-project/xz
 let tarxz = if cfg.verbose: "tar -xvf" else: "tar -xf"
 
-proc xunzip (file, trgDir :string; force :bool= false) :void=
-  when not defined(unix):
-    {.warning: &"Extracting .tar.xz files with \"tar -xf\" is only tested on unix. Use `cfg.zigSystemBin = on` or download and extract the zig compiler manually if you have issues with the local installer version."}
-  let subDir = file.lastPathPart.splitFile.name.splitFile.name  # Basename of the file, without extensions
+proc xunzip (file, trgDir :string; subDir :string= ""; force :bool= false) :void=
+  ## UnZips the given tar compatible file into trgDir, calling `tar -xf` as an execShellCmd.
+  ## Extracts into current, and then moves the generated folder into trgDir
   let xDir   = trgDir.parentDir()  # Extract dir. The tar command will create a subdir in here.
   let resDir = xDir/subDir         # Resulting dir of the tar command.
   let cmd    = &"{tarxz} {file} -C {xDir}"
@@ -26,7 +25,28 @@ proc xunzip (file, trgDir :string; force :bool= false) :void=
   resDir.copyDirWithPermissions(trgDir)
   resDir.removeDir
 
+proc zunzip (file, trgDir :string; subdir :string= ""; force :bool= false) :void=
+  ## UnZips the given zippy compatible file into trgDir.
+  ## Stores contents in a temp folder and then copies them back into trgDir, and removes the temp folder.
+  try: file.extractAll(trgDir)
+  except ZippyError: # Destination exists, so we make a temp folder and manually move the files
+    let tmpDir = trgDir/"temp"
+    if tmpDir.dirExists: tmpDir.removeDir
+    file.extractAll(tmpDir)
+    var xDir :string
+    if dirExists tmpDir/subDir: xDir = tmpDir/subDir
+    else:  # silly case for zippy not understanding extraction, so .... search for it :facepalm:
+      for dir in tmpDir.walkDir:
+        if dir.path.dirExists() and subDir in dir.path:
+          xDir = dir.path
+    echo xDir
+    xDir.copyDir(trgDir)
+    tmpDir.removeDir
+
 proc unzip *(file, trgDir :string) :void=
-  if file.endsWith(".tar.xz"): file.xunzip(trgDir)
-  else:                        file.extractAll(trgDir)
+  ## UnZips the given file into trgDir, based on its extension.
+  ## Will use tar -xf for tar.xz and zippy for any other filetype
+  let subDir = file.lastPathPart.splitFile.name.splitFile.name  # Basename of the file, without extensions
+  if file.endsWith(".tar.xz"): file.xunzip(trgDir, subDir)
+  else: file.zunzip(trgDir, subDir)
 
