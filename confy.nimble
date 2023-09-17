5de2fe5a205e45f43795cbfd7fc01d87c7b039f3
sOkam <itsfatum@gmail.com>
2023-09-17 07:16:37 +0200

    fix: new -d:cnimble. -d:nimble was conflicting with system

--- confy.nimble
+++ confy.nimble
@@ -6,7 +6,7 @@ import std/[ os,strformat ]
 #_____________________________
 # Package
 packageName   = "confy"
-version       = "0.1.4"
+version       = "0.1.5"
 author        = "sOkam"
 description   = "confy | Buildsystem for Nim & C"
 license       = "MIT"
--- src/confy/nims/task.nim
+++ src/confy/nims/task.nim
@@ -26,7 +26,7 @@ proc confy *(file :string= cfg.file.string) :void=
   ## This is the default confy task
   beforeConfy
   let builder = (&"{cfg.srcDir.string}/{file}").addFileExt(".nim")
-  let nimbl   = when defined(nimble): "-d:nimble" else: ""
+  let nimbl   = when defined(cnimble): "-d:nimble" else: ""
   sh &"{cfg.nimc} -d:ssl --outDir:{cfg.binDir.string} {builder}"   # nim -c -d:ssl --outDir:binDir srcDir/build.nim
   sh &"./{cfg.file.string.splitFile.name} {nimbl} --skipProjCfg --skipParentCfg {helper.cliParams().join(\" \")}", cfg.binDir
   afterConfy
