ecf631bf8ce81bca9911a4a7f3c20f9d9581f3b2
sOkam <7308253+heysokam@users.noreply.github.com>
2023-12-25 13:23:10 +0100

    doc: Better hello.nim example syntax

--- examples/C/hello/src/build.nim
+++ examples/C/hello/src/build.nim
@@ -4,9 +4,9 @@ let srcCore = srcDir.glob()
 let srcSub  = glob srcDir/"sub"
 var bin = Program.new(
   src   = srcCore & srcSub,
-  trg   = "hello-x64",
+  trg   = "hello",
   flags = cfg.flags(C),
 )
 
-bin.build( @["all"], run=true )
+bin.build( run=true )
 
