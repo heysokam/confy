5a3e18981e5b3359abdf2bda47c8f78c95f34caf
sOkam <7308253+heysokam@users.noreply.github.com>
2024-01-14 00:23:06 +0100

    new: Revamp dependencies management file

--- src/confy/task/deps.nim
+++ src/confy/task/deps.nim
@@ -1,80 +1,66 @@
 #:_____________________________________________________
 #  confy  |  Copyright (C) Ivan Mar (sOkam!)  |  MIT  :
 #:_____________________________________________________
-{.error:"Tasks and keywords need to be reimplemented for the refactor of 2.0.0".}
-# std dependecies
-import std/os
-import std/osproc
-import std/strformat
-import std/strscans
-# confy dependecies
+# @deps std
+from std/strformat import `&`
+from std/sets import HashSet, incl, items, len, toHashSet
+from std/sequtils import toSeq
+# @deps nstd
+from nstd/shell import withDir
+# @deps confy
+from ../cfg import nil
+import ../types
 import ../tool/strings
+import ../tool/helper
+import ../tool/logger
+import ../tool/paths
 
 
-#___________________
-type DepVers = object
-  id       :string
-  checksum :string
-type DepInfo = object
-  name     :string
-  versions :seq[DepVers]
-type Dependencies = seq[DepInfo]
-#___________________
-var firstRun :bool= on
-var depsData :Dependencies
-#___________________
-proc getInstalled () :Dependencies=
-  ## Gets the list of already installed dependecies on the system.
-  for it in execCmdEx( "nimble list -i" ).output.splitLines():
-    var tmp      :DepInfo
-    var versStr  :string
-    var versList :seq[string]
-    if scanf(it, "$*[$*]$.", tmp.name, versStr):
-      tmp.name = tmp.name.splitWhitespace().join()
-      versList = versStr.replace("(","").split("), ")
-    for ver in versList.mitems:
-      ver = ver.replace(")","")
-      var vstr   :string
-      var chksum :string
-      if scanf(ver, "version: $*, checksum: $*", vstr, chksum):
-        tmp.versions.add DepVers(id:vstr, checksum:chksum)
-    result.add tmp
-#___________________
-proc isInstalled (dep :string) :bool=
-  ## Returns true if the dependency is installed in the system.
-  ## TODO -> conditions for version management
-  for it in depsData:
-    if it.name in dep: return true
-#___________________
-proc require *(dep :string; force=false) :void {.inline.}=
-  ## Installs the given dependency using nimble
-  ## TODO Install when a new version exists  (currently downloads only when not installed)
-  if firstRun: depsData = deps.getInstalled(); firstRun = off
-  if force or not dep.isInstalled(): discard os.execShellCmd &"nimble install {dep}"
-
-
-
-
-
-
-##[
-# TODO : Currently only nimscript does this
 #_________________________________________________
-# Build Requirements list
+# @section Nim and Dependency Management
+#_____________________________
+# proc nim (opts :varargs[string,`$`]) :void=
+#   let cmd = nimGetRealBin().string&" "&opts.join(" ")
+#   dbg "Running nim with command:\n  ",cmd
+#   sh cmd, cfg.verbose
+# proc nimc (opts :varargs[string,`$`]) :void=
+#   if not fileExists(cfg.binDir/".gitignore"): writeFile(cfg.binDir/".gitignore", "*\n!.gitignore")
+#   let paths :string= if deps.len > 0: "--path:" & deps.toSeq.join(" --path:") else: ""
+#   nim &"c --outDir:{cfg.binDir} {paths} "&opts.join(" ")
+proc nimInstall *(vers :string= cfg.nim.vers) :void=
+  if cfg.nimDir.dirExists: return
+  info "Installing nim "&vers
+  git "clone", cfg.nim.url, cfg.nimDir, "-b", vers, "--depth 1"
+  withDir cfg.nimDir: sh "./build_all.sh"
+  info "Done installing nim."
+
+
+#_________________________________________________
+# @section Dependency Management
+#_____________________________
+func new *(_:typedesc[Dependencies]; deps :varargs[Dependency]) :Dependencies=  deps.toSeq.toHashSet
+  ## @descr Creates a new set of {@type Dependencies} from the given {@arg deps} dependencies list.
 #___________________
-template installRequires *()=
-  info "Installing dependencies declared with `requires`"
-  var confyID    :Natural
-  var confyFound :bool
-  for id,req in tasks.requiresData.pairs:
-    var dep :string
-    if   req == "confy"         : dep = "https://github.com/heysokam/confy@#head"; confyID = id; confyFound = true
-    elif req.endsWith("@#head") : dep = req
-    elif req.endsWith("#head")  : dep = req.replace("#head", "@#head")
-    info2 "Installing "&dep
-    exec "nimble install "&dep
-  if confyFound: system.requiresData.delete(confyID) # Remove confy requires so we dont install it multiple times
+func to *(dep :Dependency; lang :Lang) :string=
+  ## @descr Returns a string with the given {@arg dep} dependency converted to the format understood by the compiler of the given {@arg lang}.
+  case lang
+  of Nim : "--path:"&dep.dir.string
+  else:""
+func to *(deps :Dependencies; lang :Lang) :string=
+  ## @descr Returns a string with the given {@arg deps} dependencies converted to the format understood by the compiler of the given {@arg lang}.
+  case lang
+  of Nim:
+    for dep in deps: result.add " "&dep.to(lang)
+  else: result = ""
 #___________________
-template clearRequires *()=  deps.requiresData = @[]
-]##
+proc submodule *(name :string; url :string= ""; code :Dir|string= cfg.srcSub; shallow :bool= true) :Dependency {.discardable.}=
+  ## @descr Installs the given dependency as a submodule for the project
+  result = Dependency(
+    name : name,
+    url  : url,
+    src  : when code is string: code.Path else: code,
+    dir  : absolutePath( cfg.libDir/name/code )
+    ) # << Dependency( ... )
+  if not fileExists(cfg.libDir/".gitignore") : writeFile(cfg.libDir/".gitignore", "*\n!.gitignore")
+  if not dirExists(cfg.libDir/name)          : git "clone", &"{url} {cfg.libDir/name}", if shallow: " --depth 1" else: ""
 
