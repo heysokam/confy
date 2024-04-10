c8c7e1a7795322de8890708e1d0ecbe35f6f3f7d
sOkam <7308253+heysokam@users.noreply.github.com>
2024-04-10 12:28:23 +0200

    doc: Better doc-comments on tools/logger

--- src/confy/tool/logger.nim
+++ src/confy/tool/logger.nim
@@ -1,48 +1,48 @@
 #:_____________________________________________________
 #  confy  |  Copyright (C) Ivan Mar (sOkam!)  |  MIT  :
 #:_____________________________________________________
-# @deps std
-from std/strutils import join
+# @deps ndk
+import nstd/strings
 # @deps confy
 import ../types
 import ../cfg
-#___________________
-# std extensions
-func toString *(args :varargs[string]) :string=
-  ## Converts the given string varargs to a single string.
-  for arg in args:  result.add arg
 
-var stdout {.importc: "stdout", header: "<stdio.h>".} :File
-proc prnt *(args :varargs[string, `$`]) :void=  stdout.write toString(args)
-  ## Prints the input to console, without "\n" at the end.
-
-#___________________
+#_______________________________________
+# @section Logger base tools
+#_____________________________
 proc log0 *(msg :string) :void=  echo cfg.prefix, msg
-  ## Level0 log. For logging the title of a category of steps.
+  ## @descr Level0 log. For logging the title of a category of steps.
 proc log1 *(msg :string) :void=  echo cfg.tab, msg
-  ## Level1 log. For logging the submessages of a category of steps.
-
+  ## @descr Level1 log. For logging the submessages of a category of steps.
+#___________________
 proc log *(msg :varargs[string, `$`]) :void=
-  ## Reports information about the build process.
-  ## Current: echo to cli with prefix
+  ## @descr
+  ##  Reports information about the build process.
+  ##  Current: echo to cli with prefix
   log0 msg[0]
   for id,arg in msg.pairs:
     if id == 0: continue
     log1 arg
 
+#_______________________________________
+# @section Logger API
+#_____________________________
 proc info  *(msg :string) :void= log0 msg
 proc info2 *(msg :string) :void= log1 msg
-
+#___________________
 proc wrn *(args :varargs[string, `$`]) :void=  echo cfg.prefix & "! WRN ! " & args.toString
-  ## Reports a warning message to console.
-
+  ## @descr Reports a warning message to console.
+#___________________
 proc dbg *(args :varargs[string, `$`]) :void=
-  ## Reports a message to console. Does nothing if {@link cfg.verbose} is disabled.
+  ## @descr Reports a message to console. Does nothing if {@arg cfg.verbose} is disabled.
   if not cfg.verbose: return
   log0 args.join(" ")
 
+#_______________________________________
+# @section Error Management
+#_____________________________
 template cerr*(args :varargs[string, `$`]) :void=  raise newException(CompileError, args.toString)
-  ## Raises a compile exception error with the given message.
+  ## @descr Raises a compile exception error with the given message.
 template gerr*(args :varargs[string, `$`]) :void=  raise newException(GeneratorError, args.toString)
-  ## Raises a compile exception error with the given message.
+  ## @descr Raises a compile exception error with the given message.
 
