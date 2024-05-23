f2b1418f9ad4e7f633d711e3ec45d173d6f92605
sOkam <7308253+heysokam@users.noreply.github.com>
2024-05-23 18:29:48 +0200

    fmt: Move `wrapped` tools into `nstd/[ strings,paths ]`

--- src/confy/convert/make/codegen.nim
+++ src/confy/convert/make/codegen.nim
@@ -37,36 +37,6 @@ proc build *(
   ) :void=
 """
 
-#_____________________________
-func wrapped (val :string; safeWrap :bool= false) :string=
-  result.add "\""
-  result.add if not safeWrap: val else: val.replace("\\\"", "\\\\\\\"")
-  result.add "\""
-#___________________
-func wrapped (
-    path   : Path;
-    prefix : static string = "";
-    strip  : static string = "";
-  ) :string=
-  # @note This function used to be clear and clean T_T
-  let words = path.string.split(DirSep)
-  if prefix != "": result.add prefix & $DirSep
-  var skip :bool
-  for id,val in words.pairs:
-    if val == strip:
-      if id == 0: skip = true
-      continue
-    if id != 0:
-      if skip : skip = false
-      else    : result.add DirSep
-    result.add val.string.wrapped
-#___________________
-func wrapped (
-    list   : seq[Path];
-    prefix : static string = "";
-    strip  : static string = "";
-  ) :seq[string]=
-  for file in list: result.add file.wrapped(prefix, strip)
 #___________________
 func wrapped (
     list   : CodeFiles;
