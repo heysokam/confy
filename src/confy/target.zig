6208ea363c772d492d7058a60df382286eff8175
sOkam <7308253+heysokam@users.noreply.github.com>
2024-07-13 03:06:23 +0200

    rew: Minimal support for Language selection per target

--- src/confy/target.zig
+++ src/confy/target.zig
@@ -21,6 +21,12 @@ const Submodule  = @import("./submodule.zig");
 const Submodules = Submodule.List;
 const Confy      = @import("./core.zig");
 
+pub const Kind = enum { program, static, lib, unittest };
+pub const Lang = enum { None, M, Zig, C, Cpp, Nim, Asm, Unknown };
+const LangPriority = [_]Lang{ .Cpp, .Zig, .C, .M, .Nim, .Asm, .Unknown };
+const CodeList = seq(cstr);
+
+
 kind     :Kind,
 cfg      :cfg,
 builder  :*Confy,
@@ -34,10 +40,70 @@ version  :Version= zstd.version(0,0,0),
 // optim    :std.builtin.OptimizeMode= undefined,
 // priv     :BuildTrg.Private= undefined,
 
+/// @description Forces the target to be built with the given {@link Lang} when specified. Will search by file extensions otherwise.
+lang     :Lang= Lang.None,
 
-const CodeList = seq(cstr);
-pub const Kind = enum { program, static, lib, unittest };
 
+//____________________________________
+/// @section Language Management tools
+//____________________________
+const lang = struct {
+  /// @descr Returns the language of the {@arg ext} extension. An empty extension will return Unknown lang.
+  fn fromExt (ext :cstr) Lang {
+    const result = if (std.mem.eql(u8, ext, "cpp")) Lang.Cpp
+      else if (std.mem.eql(u8, ext, ".cc" )) Lang.Cpp
+      else if (std.mem.eql(u8, ext, ".c"  )) Lang.C
+      else if (std.mem.eql(u8, ext, ".cm" )) Lang.M
+      else if (std.mem.eql(u8, ext, ".zm" )) Lang.M
+      else if (std.mem.eql(u8, ext, ".zig")) Lang.Zig
+      else if (std.mem.eql(u8, ext, ".nim")) Lang.Nim
+      else if (std.mem.eql(u8, ext, ".s"  )) Lang.Asm
+      else Lang.Unknown;
+    return result;
+  }
+
+  /// @descr Returns the language of the {@arg file}, based on its extension. An empty extension will return Unknown lang.
+  fn fromFile (file :cstr) Lang { return lang.fromExt(std.fs.path.extension(file)); }
+
+  /// @descr Returns the preferred language of the {@arg src} CodeList, based on the extension priorities for each lang.
+  /// @note List of files that contain C++ code will be assumed to be targeting a C++ compiler, even if they combine C code in them.
+  /// @note List of files that contain Zig code will be assumed to be targeting a Zig compiler, even if they combine C code in them.
+  fn get (src :CodeList) Lang {
+    var langs = std.EnumSet(Lang).initEmpty();
+    for (src.items) | file | langs.insert(lang.fromFile(file));
+    if (langs.count() == 1) {
+      var it = langs.iterator(); return it.next().?;
+    } else {
+      for (LangPriority) |L| {
+        if (langs.contains(L)) return L;
+      }
+    }
+    return Lang.Unknown;
+  }
+};
+
+
+
+
+// TODO:
+// proc findExt *(file :DirFile) :string=
+//   ## @descr
+//   ##  Finds the extension of a file that is sent without it.
+//   ##  Walks the file's dir, and matches all entries found against the full path of the given input file.
+//   ## @raises IOError if the file does have an extension already.
+//   if file.file.string.splitFile.ext != "": raise newException(IOError, &"Tried to find the extension of a file that already has one.\n  {file.dir/file.file}")
+//   let filepath = file.dir/file.file
+//   for found in file.dir.string.walkDir:
+//     if found.kind == pcDir: continue
+//     if filepath.string in found.path: return found.path.splitFile.ext
+//   raise newException(IOError, &"Failed to find the extension of file:\n  {file.dir/file.file}")
+
+
+
+
+//____________________________________
+// @section Build Target Management tools
+//____________________________
 const BuildTrg_args = struct {
   trg     : cstr,
   src     : ?cstr_List= null,
@@ -46,6 +112,7 @@ const BuildTrg_args = struct {
   sub     : cstr  = "",
   deps    : ?[]const Submodule= null,
   version : cstr  = "0.0.0",
+  lang    : ?Lang = null,
 };
 
 //_____________________________________
@@ -70,6 +137,7 @@ pub fn new(
   if (args.entry != null) try result.src.append(args.entry.?);
   if (args.src  != null) { for (args.src.?)  | file | { try result.src.append(file); } }
   if (args.deps != null) { for (args.deps.?) | dep  | { try result.deps.append(dep); } }
+  result.lang = args.lang orelse BuildTrg.lang.get(result.src);
   return result;
 }
 //_____________________________________
@@ -83,45 +151,66 @@ pub fn StaticLib (args :BuildTrg_args, confy :*Confy) !BuildTrg { return BuildTr
 pub fn UnitTest  (args :BuildTrg_args, confy :*Confy) !BuildTrg { return BuildTrg.new(BuildTrg.Kind.unittest, args, confy); }
 
 
-//_____________________________________
-fn getZigBin (trg :*const BuildTrg) !cstr {
-  return try std.fs.path.join(trg.builder.A.allocator(), &.{trg.cfg.dir.bin, trg.cfg.dir.zig, "zig"});
-}
-fn getCacheDir (trg :*const BuildTrg) !cstr {
-  return try std.fs.path.join(trg.builder.A.allocator(), &.{trg.cfg.dir.bin, trg.cfg.dir.cache, "zig"});
-}
 fn getBin (trg :*const BuildTrg) !cstr {
   return switch(trg.kind) {
     .unittest => try std.fs.path.join(trg.builder.A.allocator(), &.{trg.cfg.dir.bin, "test"}),
     else      => try std.fs.path.join(trg.builder.A.allocator(), &.{trg.cfg.dir.bin, trg.sub, trg.trg}),
     };
 }
-fn getEmitBin (trg :*const BuildTrg) !cstr {
-  return try std.fmt.allocPrint(trg.builder.A.allocator(), "-femit-bin={s}", .{try trg.getBin()});
-}
+
+const zig = struct {
+  //_____________________________________
+  fn getCC (trg :*const BuildTrg) !cstr {
+    return try std.fs.path.join(trg.builder.A.allocator(), &.{trg.cfg.dir.bin, trg.cfg.dir.zig, "zig"});
+  }
+  fn getCacheDir (trg :*const BuildTrg) !cstr {
+    return try std.fs.path.join(trg.builder.A.allocator(), &.{trg.cfg.dir.bin, trg.cfg.dir.cache, "zig"});
+  }
+  fn getEmitBin (trg :*const BuildTrg) !cstr {
+    return try std.fmt.allocPrint(trg.builder.A.allocator(), "-femit-bin={s}", .{try trg.getBin()});
+  }
+  //_____________________________________
+  /// @descr Orders confy to build the resulting binary for this Zig BuildTrg.
+  fn build (trg :*const BuildTrg) !void {
+    // Create the compilation command
+    var args = seq(cstr).init(trg.builder.A.allocator());
+    try args.append(try zig.getCC(trg));
+    switch (trg.kind) {
+     .program     => try args.append("build-exe"),
+     .lib,.static => try args.append("build-lib"),
+     .unittest    => try args.append("test"),
+    }
+    // Add the cache folder args
+    const cache = try zig.getCacheDir(trg);
+    try args.appendSlice(&.{"--cache-dir", cache, "--global-cache-dir", cache});
+    // Add the binary output
+    try args.append(try zig.getEmitBin(trg));
+    // Add the code
+    try args.appendSlice(trg.src.items);
+    // Report to CLI and build
+    echo("ᛝ confy: Building target binary ...");
+    try zstd.shell.run(args.items, trg.builder.A.allocator());
+    echo("ᛝ confy: Done Building.");
+  }
+};
+
+const C = struct {
+  //_____________________________________
+  /// @descr Orders confy to build the resulting binary for this C BuildTrg.
+  fn build (trg :*const BuildTrg) !void {
+    _=trg;
+  }
+};
 
 //_____________________________________
 /// @descr Orders confy to build the resulting binary for this BuildTrg.
 pub fn build (trg :*const BuildTrg) !void {
-  // Create the compilation command
-  var args = seq(cstr).init(trg.builder.A.allocator());
-  try args.append(try trg.getZigBin());
-  switch (trg.kind) {
-   .program     => try args.append("build-exe"),
-   .lib,.static => try args.append("build-lib"),
-   .unittest    => try args.append("test"),
+  // @note Sends the control flow of the builder into the relevant function to build the resulting binary for this BuildTrg.
+  switch (trg.lang) {
+    .Zig => try zig.build(trg),
+    .C   => try C.build(trg),
+    else => unreachable,
   }
-  // Add the cache folder args
-  const cache = try trg.getCacheDir();
-  try args.appendSlice(&.{"--cache-dir", cache, "--global-cache-dir", cache});
-  // Add the binary output
-  try args.append(try trg.getEmitBin());
-  // Add the code
-  try args.appendSlice(trg.src.items);
-  // Report to CLI and build
-  echo("ᛝ confy: Building target binary ...");
-  try zstd.shell.run(args.items, trg.builder.A.allocator());
-  echo("ᛝ confy: Done Building.");
 }
 
 //_____________________________________
