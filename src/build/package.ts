eb7ef1a34071ebb732903ed4dcecf5dd1072ecb9
sOkam <7308253+heysokam@users.noreply.github.com>
2025-03-22 21:55:04 +0100

    bld: Builder script for creating confy/`package.json` from `cfg.tool`

---
+++ src/build/package.ts
@@ -0,0 +1,21 @@
+//:______________________________________________________________________
+//  ᛝ confy  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
+//:______________________________________________________________________
+/**
+ * @fileoverview
+ * Automatically updates confy's package.json using its internal configuration.
+ * Should be run during confy's build process.
+ * */
+// @deps confy
+import pkg from '../../package.json'
+import { File } from '../confy/tools/files'
+import { cfg } from '../confy/cfg'
+
+const icon         = (!cfg.tool.icon ) ? cfg.tool.icon  : `${cfg.tool.icon} `
+const description  = (!cfg.tool.descr) ? cfg.tool.descr : ` ${cfg.tool.separator.descr} ${cfg.tool.descr}`
+const result       = structuredClone(pkg)
+result.name        = cfg.tool.name
+result.version     = cfg.tool.version
+result.description = `${icon}${cfg.tool.name}${description}`
+File.write("./package.json", JSON.stringify(result, null, 2))
+
