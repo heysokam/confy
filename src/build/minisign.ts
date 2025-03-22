dad1a840d9f0687ebd99e7d44082ba74a8c40691
sOkam <7308253+heysokam@users.noreply.github.com>
2025-03-22 11:42:55 +0100

    bld: Auto-downloader for `@mlugg/setup-zig/minisign.js`

---
+++ src/build/minisign.ts
@@ -0,0 +1,24 @@
+//:______________________________________________________________________
+//  ᛝ confy  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
+//:______________________________________________________________________
+/**
+ * @fileoverview
+ * Automatically downloads minisign.js from @mlugg/setup-zig into @confy/tools
+ * Should be run during confy's build process.
+ *
+ * @note
+ * Ideally minisign should be an npm package instead, but :shrug:
+ * */
+// @deps confy
+import { info } from "@confy/log"
+import { File } from "@confy/tools"
+
+const url = new URL("https://raw.githubusercontent.com/mlugg/setup-zig/refs/heads/main/minisign.js")
+const trgDir  = "./src/confy/tools/"
+const trgFile = trgDir+"minisign.js"
+
+if (import.meta.main) try { run() } catch { throw new Error("[FATAL]: Failed to build MiniSign from: "+url)}
+async function run() :Promise<void> {
+  info("Build: Downloading @mlugg/setup-zig/minisign.js to "+trgFile)
+  await File.download(url, trgDir)
+}
