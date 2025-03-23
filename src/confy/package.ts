2e30939e37296e63f988fd5d8f8197c93459f434
sOkam <7308253+heysokam@users.noreply.github.com>
2025-03-23 22:07:43 +0100

    fix: Calls to `get.Bun` not passing a `Config` object

--- src/confy/package.ts
+++ src/confy/package.ts
@@ -5,6 +5,9 @@
 //_____________________________________________|
 import { Manager } from '@confy/manager'
 import { File } from '@confy/tools/files'
+import { cfg as confy } from './cfg'
+
+const cfg = confy.defaults.clone()
 
 export namespace Package {
   export type Info = any
@@ -27,8 +30,8 @@ export namespace Package {
     if (Package.has.info()) return  // FIX: Should check if Package.info has the dependencies instead
     const pkg = await Package.get.info()
     if (pkg.name === "confy") return
-    await Manager.Bun.run("add", "confy")
-    await Manager.Bun.run("install")
+    await Manager.Bun.run(cfg, "add", "confy")
+    await Manager.Bun.run(cfg, "install")
   }
 }
 
