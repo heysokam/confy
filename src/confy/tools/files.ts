14d8e3829fd164c1d6b508ad2622c0d62d114228
sOkam <7308253+heysokam@users.noreply.github.com>
2025-03-24 14:04:46 +0100

    new: Support for `.tar.xz` extraction

--- src/confy/tools/files.ts
+++ src/confy/tools/files.ts
@@ -7,16 +7,19 @@ import * as path from 'path'
 import { Readable } from 'stream'
 // @deps lib
 import extract from 'extract-zip'
+import * as tar from 'tar'
+import * as XZ from 'xz-decompress'
 // @deps confy
 import { Default as log } from '@confy/log'
+import { pathToFileURL } from 'url'
 
 
 export const Path = {
-  ext      : (P :fs.PathLike) :fs.PathLike=> path.extname(P.toString()),
-  basename : (P :fs.PathLike) :fs.PathLike=> path.basename(P.toString()),
+  ext      : (P :fs.PathLike) :string=> path.extname(P.toString()),
+  basename : (P :fs.PathLike, ext ?:fs.PathLike) :fs.PathLike=> path.basename(P.toString(), (ext) ? ext.toString() : undefined),
   dirname  : (P :fs.PathLike) :fs.PathLike=> path.dirname(P.toString()),
   exists   : (path :fs.PathLike) :boolean => { return fs.existsSync(path) },
-  rm       : (path :fs.PathLike) => { fs.rm(path, () => {}) },
+  rm       : (path :fs.PathLike) => fs.rm(path, () => {}),
   join     : (...paths:fs.PathLike[]) :fs.PathLike=> path.join(...paths.flatMap(a => a.toString())),
 
   /**
@@ -40,6 +43,7 @@ export const Path = {
    *
    * @note
    * There will always be an implicit `process.cwd()` at the start.
+   *
    * Use {@link path.resolve} to solve non-cwd paths.
    * */
   toAbsolute: (...paths :fs.PathLike[]) :fs.PathLike=> path.resolve(process.cwd(), ...paths.map((p)=>p.toString())),
@@ -48,9 +52,13 @@ export const Path = {
 
 export const Dir = {
   exists : Path.exists,
+  rmv    : (path :fs.PathLike) => fs.rmdir(path, () => {}),
   cwd    : () :fs.PathLike => { return process.cwd() },
-  move   : (src :fs.PathLike, trg :fs.PathLike) => fs.cpSync(src as string, trg as string),
   create : (trg :fs.PathLike, recursive :boolean= true) => fs.mkdirSync(trg, {recursive: recursive}),
+  move   : (src :fs.PathLike, trg :fs.PathLike, opts ?:fs.CopySyncOptions) => {
+    fs.cpSync(src.toString(), trg.toString(), {...opts, recursive: true})
+    Dir.rmv(src)
+  },
 } //:: Dir
 
 
@@ -71,6 +79,65 @@ export const File = {
    * Type Wrapper for {@link extract-zip}/extract
    * */
   unzip: async(trg :fs.PathLike, opts :extract.Options) :Promise<void>=> extract(trg.toString(), opts),
+
+  /**
+   * @description
+   * Wrapper for {@link tar}/extract
+   * @param src (ergonomics/clarity) Overrides opts.f when passed as a non-nullish value
+   * @param dir (ergonomics/clarity) Folder where the contents will be extracted to. Overrides opts.C when passed
+   * */
+  untar: async(
+      src   : fs.PathLike,
+      dir   : fs.PathLike,
+      opts  : tar.TarOptionsWithAliasesAsyncFile,
+    ) :Promise<void>=> tar.extract({...opts, f: src ?? opts.f, C: dir ?? opts.C} as tar.TarOptionsWithAliasesAsyncFile),
+
+  /**
+   * @description
+   * Reads the data at {@param src}, decompresses it, and extracts its contents to the {@param trg} folder
+   * Assumes that the file stored at `src` contains data compressed/archived as lzma/XZ _(ie: .tar.xz)_
+   * eg: If `src` is `path/file.tar.xz` file, its contents will be extracted to trg/*
+   *
+   * @param src Overrides opts.f when passed as a non-nullish value when unarchiving from `@param tmp`
+   * @param dir Folder where the contents will be extracted to. When passed, it overrides opts.C when unarchiving from `@param tmp`
+   * @param tmp Path where the `.tar` data will be decompressed into. Will be passed as input to {@link File.untar}
+   * @param clean Removes the temporary file at {@param tmp} when done unarchiving
+   * @param opts Options forwarded to {@link File.untar}
+   * */
+  untarxz: async(
+      src   : fs.PathLike,
+      dir   : fs.PathLike,
+      tmp   : fs.PathLike,
+      clean : boolean = true,
+      opts  = {} as tar.TarOptionsWithAliasesAsyncFile,
+    ) :Promise<void>=> {
+    log.verb("Decompressing XZ file: ", src, " -> ", tmp)
+    await File.lzma.decompress(src, tmp)
+    log.verb("Done decompressing: ", src)
+    log.verb("Extracting tar file: ", tmp,  " -> ", dir)
+    await File.untar(tmp, dir, opts)
+    log.verb("Done extracting: ", tmp)
+    if (clean) File.rmv(tmp)
+  },
+
+
+  lzma: {
+    /**
+     * @description
+     * Wrapper for {@link lzma}/decompress
+     * Reads the data at {@param src}, decompresses it, and outputs the result to {@param trg}
+     * eg: If `src` was a .tar.xz file, the result will be a .tar file.
+     * */
+    decompress: async(
+        src : fs.PathLike,
+        trg : fs.PathLike,
+      ) :Promise<void>=> {
+      const data   = await fetch(pathToFileURL(src.toString()))
+      const stream = new XZ.XzReadableStream(data.body!)
+      const view = new DataView(await new Response(stream).arrayBuffer())
+      File.write(trg, view)
+    },
+  },
 
   /**
    * @description
