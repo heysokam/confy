1158fb49d2efc1725cf933db4ca749c6cf83fd0c
sOkam <7308253+heysokam@users.noreply.github.com>
2025-03-25 15:58:51 +0100

    new: `shell.exec` for calling a shell command directly
    todo: remove Bun.spawn from `shell.run`

--- src/confy/tools/shell.ts
+++ src/confy/tools/shell.ts
@@ -1,9 +1,15 @@
 //:______________________________________________________________________
 //  ᛝ confy  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
 //:______________________________________________________________________
+import * as node from 'child_process'
 
 export async function run (...args:unknown[]) {
   const proc = Bun.spawn([...(args as string[])], {stdout: "inherit", stderr: "inherit"})
   return await proc.exited
 }
 
+export function exec (...args:string[]) {
+  const cmd  = args.toString()
+  const proc = node.exec(cmd, {shell: "sh"} as node.ExecOptions, undefined)
+  return proc.exitCode
+}
