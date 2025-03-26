19a3f675a2880ece54a68d34ceb248f0da8ace78
sOkam <7308253+heysokam@users.noreply.github.com>
2025-03-26 12:23:45 +0100

    chg: Improve `tools/shell` functions for shell commands (todo:windows)

--- src/confy/tools/shell.ts
+++ src/confy/tools/shell.ts
@@ -1,15 +1,40 @@
 //:______________________________________________________________________
 //  ᛝ confy  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
 //:______________________________________________________________________
-import * as node from 'child_process'
+import * as util from 'util'
+import * as os from 'child_process'
+export namespace shell {
+
 
 export async function run (...args:unknown[]) {
+  // FIX: Change to node.spawn
   const proc = Bun.spawn([...(args as string[])], {stdout: "inherit", stderr: "inherit"})
   return await proc.exited
-}
+} //:: shell.run
 
-export function exec (...args:string[]) {
-  const cmd  = args.toString()
-  const proc = node.exec(cmd, {shell: "sh"} as node.ExecOptions, undefined)
-  return proc.exitCode
-}
+
+export async function exec_node (opts :os.ExecOptions, ...args:string[]) {
+  const cmd  = args.join(" ")
+  const proc = util.promisify(os.exec)
+  const out  = await proc(cmd, opts)
+  return out
+} //:: shell.exec
+
+export async function exec (opts :any, ...args:string[]) {
+  // FIX: Change to node.spawn
+  const proc = Bun.spawn(["sh", ...(args as string[])], {stdout: "inherit", stderr: "inherit", ...opts})
+  return await proc.exited
+} //:: shell.exec
+
+export function sh (...args:string[]) {
+  return exec({} as os.ExecOptions, ...args)
+} //:: shell.sh
+
+} //:: tools/shell
+
+
+// Alternative exports
+export const run  = shell.run
+export const exec = shell.exec
+export const sh   = shell.sh
+
