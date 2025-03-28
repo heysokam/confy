8ce4827a4e378d32543f187a832536c1d91422cd
sOkam <7308253+heysokam@users.noreply.github.com>
2025-03-28 04:15:08 +0100

    bld: Format version.test.ts to have `new Version` usage at the top

--- src/confy/tools/version.test.ts
+++ src/confy/tools/version.test.ts
@@ -1,49 +1,11 @@
 //:______________________________________________________________________
 //  ᛝ confy  |  Copyright (C) Ivan Mar (sOkam!)  |  GNU GPLv3 or later  :
 //:______________________________________________________________________
+// @deps tests
 import { describe, expect, it, spyOn } from 'bun:test'
-
+// @deps version
 import { Version, version } from './version'
 
-describe("version.parse", () => {
-  describe("simple case", () => {
-    const simple :Version= version.parse("1.2.3")
-    it("should parse the major field as expected", () => { expect(simple.major).toBe(   1) })
-    it("should parse the minor field as expected", () => { expect(simple.minor).toBe(   2) })
-    it("should parse the patch field as expected", () => { expect(simple.patch).toBe(   3) })
-    it("should parse the tag   field as expected", () => { expect(simple.tag  ).toBe(null) })
-    it("should parse the build field as expected", () => { expect(simple.build).toBe(null) })
-  })
-
-  describe.each(version.Specification.Invalid)("Invalid cases :: %s", (entry) => {
-    it("should error when parsing", () => { expect(() => version.parse(entry)).toThrowError() })
-  })
-
-  describe.each(version.Specification.Valid)("complex cases :: %s", (...entry) => {
-    const simple :Version= version.parse(entry[0] as string)
-    it("should parse the major field as expected", () => { expect(simple.major).toBe(entry[1] as number) })
-    it("should parse the minor field as expected", () => { expect(simple.minor).toBe(entry[2] as number) })
-    it("should parse the patch field as expected", () => { expect(simple.patch).toBe(entry[3] as number) })
-    it("should parse the tag   field as expected", () => { expect(simple.tag  ).toBe(entry[4] as string|null  ) })
-    it("should parse the build field as expected", () => { expect(simple.build).toBe(entry[5] as string|null) })
-  })
-})
-
-describe("version.from", () => {
-  const M = 42
-  const m = 21
-  const p = 10
-  const t = "alpha"
-  const b = "dev.1234"
-  const result :Version= version.from(M,m,p,t,b)
-  it("should assign `@param M` to the `.major` field of the result",        () => { expect(result.major).toBe(M) })
-  it("should assign `@param m` to the `.minor` field of the result",        () => { expect(result.minor).toBe(m) })
-  it("should assign `@param p` to the `.patch` field of the result",        () => { expect(result.patch).toBe(p) })
-  it("should assign `@param t` to the `.tag`   field of the result",        () => { expect(result.tag  ).toBe(t) })
-  it("should assign `@param b` to the `.build` field of the result",        () => { expect(result.build).toBe(b) })
-  it("should assign null to the `.tag`   field of the result when omitted", () => { expect(version.from(1,2,3).tag  ).toBe(null) })
-  it("should assign null to the `.build` field of the result when omitted", () => { expect(version.from(1,2,3).build).toBe(null) })
-})
 
 describe("Version", () => {
   it("should return a 0.0.0 version when `@param M` is null", () => {
@@ -84,4 +46,45 @@ describe("Version", () => {
     expect(result).toBe(Expected)
   })
 }) //:: Version
+
+
+describe("version.parse", () => {
+  describe("simple case", () => {
+    const simple :Version= version.parse("1.2.3")
+    it("should parse the major field as expected", () => { expect(simple.major).toBe(   1) })
+    it("should parse the minor field as expected", () => { expect(simple.minor).toBe(   2) })
+    it("should parse the patch field as expected", () => { expect(simple.patch).toBe(   3) })
+    it("should parse the tag   field as expected", () => { expect(simple.tag  ).toBe(null) })
+    it("should parse the build field as expected", () => { expect(simple.build).toBe(null) })
+  })
+
+  describe.each(version.Specification.Invalid)("Invalid cases :: %s", (entry) => {
+    it("should error when parsing", () => { expect(() => version.parse(entry)).toThrowError() })
+  })
+
+  describe.each(version.Specification.Valid)("complex cases :: %s", (...entry) => {
+    const simple :Version= version.parse(entry[0] as string)
+    it("should parse the major field as expected", () => { expect(simple.major).toBe(entry[1] as number) })
+    it("should parse the minor field as expected", () => { expect(simple.minor).toBe(entry[2] as number) })
+    it("should parse the patch field as expected", () => { expect(simple.patch).toBe(entry[3] as number) })
+    it("should parse the tag   field as expected", () => { expect(simple.tag  ).toBe(entry[4] as string|null  ) })
+    it("should parse the build field as expected", () => { expect(simple.build).toBe(entry[5] as string|null) })
+  })
+})
+
+describe("version.from", () => {
+  const M = 42
+  const m = 21
+  const p = 10
+  const t = "alpha"
+  const b = "dev.1234"
+  const result :Version= version.from(M,m,p,t,b)
+  it("should assign `@param M` to the `.major` field of the result",        () => { expect(result.major).toBe(M) })
+  it("should assign `@param m` to the `.minor` field of the result",        () => { expect(result.minor).toBe(m) })
+  it("should assign `@param p` to the `.patch` field of the result",        () => { expect(result.patch).toBe(p) })
+  it("should assign `@param t` to the `.tag`   field of the result",        () => { expect(result.tag  ).toBe(t) })
+  it("should assign `@param b` to the `.build` field of the result",        () => { expect(result.build).toBe(b) })
+  it("should assign null to the `.tag`   field of the result when omitted", () => { expect(version.from(1,2,3).tag  ).toBe(null) })
+  it("should assign null to the `.build` field of the result when omitted", () => { expect(version.from(1,2,3).build).toBe(null) })
+})
 
