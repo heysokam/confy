137fd88b80948fad74510087db00a35874d880fe
sOkam <7308253+heysokam@users.noreply.github.com>
2025-03-31 17:20:48 +0200

    new: Expose new usercode tools on the entrypoint

--- src/confy.nim
+++ src/confy.nim
@@ -6,4 +6,7 @@
 ##!  Confy : Comfortable and Configurable Buildsystem
 ##!  @todo : Docs here
 #_____________________________________________________|
-from ./confy/target import nil ; export target
+from ./confy/target        import nil ; export target
+from ./confy/package       import nil ; export package
+from ./confy/dependency    import nil ; export dependency
+from ./confy/tools/version import nil ; export version
