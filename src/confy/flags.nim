018d7790688553b310bff4dbf8e120ee299adc47
sOkam <7308253+heysokam@users.noreply.github.com>
2025-05-12 19:54:28 +0200

    chg: Silence c++98 compat errors for C and C++

--- src/confy/flags.nim
+++ src/confy/flags.nim
@@ -22,12 +22,18 @@ const Strict * = @[
   "-pedantic",
   "-pedantic-errors",
   "-Wno-declaration-after-statement",
+  # Warn of dodgy situations
   "-Wno-error=vla",
   "-Wno-error=padded",
   "-Wno-error=pre-c2x-compat",
   "-Wno-error=unsafe-buffer-usage",
   "-Wno-error=#warnings",
+  "-Wno-error=documentation",
   "-Wno-error=documentation-unknown-command",
+  # Silence Errors for perfectly valid modern code
+  "-Wno-c++98-c++11-c++14-c++17-compat", "-Wno-c++98-c++11-c++14-c++17-compat-pedantic",  "-Wno-c++98-c++11-c++14-compat",    "-Wno-c++98-c++11-c++14-compat-pedantic",
+  "-Wno-c++98-c++11-compat",             "-Wno-c++98-c++11-compat-binary-literal",        "-Wno-c++98-c++11-compat-pedantic",
+  "-Wno-c++98-compat",                   "-Wno-c++98-compat-bind-to-temporary-copy",      "-Wno-c++98-compat-extra-semi",     "-Wno-c++98-compat-local-type-template-args",      "-Wno-c++98-compat-pedantic", "-Wno-c++98-compat-unnamed-type-template-args",
   ]
 #___________________
 func std *(lang :build.Lang) :build.Flag=
